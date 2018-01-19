7bfb930afd0d9a5570b9bef16e567bd78ec56382
jieeraoyun <jieeraoyun@qq.com>
2018-01-19 19:30:05 +0800

    添加部分wifidirect功能

--- app/src/main/java/com/videolive/video/SendReady.java
+++ app/src/main/java/com/videolive/video/SendReady.java
@@ -2,8 +2,16 @@ package com.videolive.video;
 
 import android.content.Intent;
 import android.hardware.Camera;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pGroup;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -17,6 +25,11 @@ import android.widget.Toast;
 import com.library.live.vd.VDEncoder;
 import com.videolive.R;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 
 import static android.hardware.Camera.CameraInfo.CAMERA_FACING_FRONT;
@@ -35,11 +48,12 @@ public class SendReady extends AppCompatActivity {
     private Button begin;
     private Spinner sb_preview;
     private List<Camera.Size> previewSIzes;
-    private String [] list;
     private ArrayAdapter<String> adapter;
     private Camera.Size sizeNow = null;
     private int cameraNowId = 0;
     private int frontCamId = 1;
+    private Spinner sb_wifi_direct;
+    private WifiP2pDevice[] mWifiP2pDeviceList;
 
 
     @Override
@@ -119,7 +133,81 @@ public class SendReady extends AppCompatActivity {
             }
         });
 
+        sb_wifi_direct = findViewById(R.id.sb_wifi_direct);
+        initWifiDirect();
 
+    }
+
+
+    public void initWifiDirect(){
+        sb_wifi_direct.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                url.setText(getIPFromMac(mWifiP2pDeviceList[position].deviceAddress));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        WifiP2pManager mManager = (WifiP2pManager) getSystemService(WIFI_P2P_SERVICE);
+        WifiP2pManager.Channel mChannel = mManager.initialize(this, Looper.myLooper(), null);
+        mManager.requestConnectionInfo(mChannel, new WifiP2pManager.ConnectionInfoListener() {
+            @Override
+            public void onConnectionInfoAvailable(WifiP2pInfo info) {
+                if(!info.isGroupOwner){
+                    sb_wifi_direct.setEnabled(false);
+                    if(info.groupOwnerAddress!=null) {
+                        url.setText(info.groupOwnerAddress.getHostAddress());
+                    }
+                } else {
+                    sb_wifi_direct.setEnabled(true);
+                }
+            }
+        });
+
+        mManager.requestGroupInfo(mChannel, new WifiP2pManager.GroupInfoListener() {
+            @Override
+            public void onGroupInfoAvailable(WifiP2pGroup group) {
+                if (group!=null && group.isGroupOwner()){
+                    mWifiP2pDeviceList = new WifiP2pDevice[group.getClientList().size()];
+                    mWifiP2pDeviceList =group.getClientList().toArray(mWifiP2pDeviceList);
+                    String[] list = new String[mWifiP2pDeviceList.length];
+                    for (int i=0;i<mWifiP2pDeviceList.length;i++) {
+                        list[i]=mWifiP2pDeviceList[i].deviceName+":"+mWifiP2pDeviceList[i].deviceAddress;
+                    }
+                    ArrayAdapter adapter = new ArrayAdapter<String>(SendReady.this,android.R.layout.simple_spinner_item, list);
+                    sb_wifi_direct.setAdapter(adapter);
+                    if(list.length>0)sb_wifi_direct.setSelection(0);
+                }
+            }
+        });
+    }
+
+    public static String getIPFromMac(String MAC) {
+        String mMac = MAC.trim().toLowerCase();
+        BufferedReader br = null;
+        try {
+
+            br = new BufferedReader(new FileReader("/proc/net/arp"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.toLowerCase().trim();
+                if (line.contains(mMac)) {
+                    return line.split(" ")[0];
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 
     private void start() {
@@ -164,8 +252,8 @@ public class SendReady extends AppCompatActivity {
         Camera camera = Camera.open(camId);
         Camera.Parameters params = camera.getParameters();
         previewSIzes = params.getSupportedPreviewSizes();
-        if(adapter != null-m )adapter.notifyDataSetInvalidated();
-        list = new String[previewSIzes.size()];
+        if(adapter != null)adapter.notifyDataSetInvalidated();
+        String[] list = new String[previewSIzes.size()];
         for(int i = 0 ;i < previewSIzes.size();i++){
             Camera.Size size = previewSIzes.get(i);
             list[i] = size.width+"*"+size.height;
